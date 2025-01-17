ce3d8b2b6721bfd698414caae9941d35d5e41178
Uwakmfon Akpan <astongemmy@gmail.com>
2025-01-17 13:17:48 +0100

    Added Dockerfile

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
+import { ScrollToTopController } from '@algrith/scroll-to-top';
 import React, { useEffect, useRef } from 'react';
+import '@algrith/scroll-to-top/dist/index.css';
 
-import useToggleScrollToTopController from '../hooks/useToggleScrollToTopController';
 import useResizeHeaderOnScroll from '../hooks/useResizeHeaderOnScroll';
 import useHideCurrentPageLink from '../hooks/useHideCurrentPageLink';
 import useScrollToElement from '../hooks/useScrollToElement';
@@ -28,11 +29,6 @@ const Layout = (props) => {
 	useHideCurrentPageLink();
 	useRippleEffect();
 	
-	useToggleScrollToTopController({
-		selector: '#back-to-top',
-		display: 'flex'
-	});
-	
 	useToggleNavbar({
 		closer: closeMenuRef,
 		opener: openMenuRef,
@@ -43,18 +39,17 @@ const Layout = (props) => {
 	return (
 		<div>
 			<Header openMenuRef={openMenuRef} />
+      
+      <span className="ripple-node cursor-pointer absolute justify-center items-center right-8 md:right-12 lg:right-32 -bottom-8 rounded-full bg-theme-primary text-white text-3xl w-16 h-16 md:w-18 md:h-18 z-10">
+        Button
+      </span>
 			<Navbar closeMenuRef={closeMenuRef} navbarRef={navbarRef} />
 			<ThemeSwitch />
 			
 			{props.children}
 			
 			<Footer />
-			
-			<span id="back-to-top" className="scroll-selector cursor-pointer hidden fixed justify-center items-center right-8 md:right-12 bottom-8 rounded-full bg-theme-primary ripple-node text-white text-3xl w-16 h-16 md:w-18 md:h-18">
-				<svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M7 11l5-5m0 0l5 5m-5-5v12" />
-				</svg>
-			</span>
+      <ScrollToTopController />
 			
 			<div ref={overlayRef} className="fixed hidden top-0 left-0 z-20 bg-black opacity-50 h-screen w-screen"></div>
 
