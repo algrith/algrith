32adc6b7e2a34b81a7814759c8aeedb0cf32fa5a
Uwakmfon Akpan <astongemmy@gmail.com>
2025-02-10 13:08:17 +0100

    Removed next-pwa from package.json

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,11 @@
 // export default nextConfig;
 
 import removeImports from 'next-remove-imports';
-import withPWA from 'next-pwa';
 
 import withTwin from './utils/twin.mjs';
 
 export default removeImports()(
   withTwin({
     reactStrictMode: true,
-    ...withPWA({
-      disable: process.env.NODE_ENV === 'development',
-      skipWaiting: true,
-      register: true,
-      dest: 'public'
-    })
   })
 );
