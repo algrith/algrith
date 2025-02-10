24ec240fff7efe6d59a8ffd1c128ed3303efe1b9
Uwakmfon Akpan <astongemmy@gmail.com>
2025-02-10 12:50:31 +0100

    Updated next config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+import removeImports from 'next-remove-imports';
+import withPWA from 'next-pwa';
+
+import withTwin from './utils/twin.mjs';
+
+export default removeImports()(
+  withTwin({
+    reactStrictMode: true,
+    ...withPWA({
+      disable: process.env.NODE_ENV === 'development',
+      skipWaiting: true,
+      register: true,
+      dest: 'public'
+    })
+  })
+);
