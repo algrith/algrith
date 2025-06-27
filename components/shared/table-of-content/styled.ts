bca50c554c91899087b121f4bdd1c9733ac45ba3
Uwakmfon Akpan <astongemmy@gmail.com>
2025-06-27 12:54:11 +0100

    Updated projects data and UI.

--- components/shared/table-of-content/styled.ts
+++ components/shared/table-of-content/styled.ts
@@ -2,7 +2,7 @@ import tw, { styled } from 'twin.macro';
 
 export const TableOfContentWrapper = styled.div`
   .content-wrapper {
-    ${tw`px-6 pt-8 md:py-12 left-0 flex flex-col gap-4 bg-white dark:bg-dark-mode-secondary -bottom-full md:bottom-0 w-full md:w-auto h-[50vh] md:h-auto fixed md:sticky md:top-20 rounded-t-2xl rounded-b-none md:rounded-2xl transition-all duration-300 ease-in-out`};
+    ${tw`px-6 pt-8 md:py-12 left-0 flex flex-col gap-4 bg-white overflow-y-auto dark:bg-dark-mode-secondary -bottom-full md:bottom-0 w-full md:w-auto h-[50vh] md:h-auto fixed md:sticky md:top-20 rounded-t-2xl rounded-b-none md:rounded-2xl transition-all duration-300 ease-in-out`};
 
     &.show {
       ${tw`bottom-0 z-[21]`};
