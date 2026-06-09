0ebace38593172f25ecffa8780b3abeb34d80ae1
Uwakmfon Akpan <astongemmy@gmail.com>
2026-06-09 19:41:31 +0100

    Updated sitemap file.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,46 +2,88 @@ import type { MetadataRoute } from 'next';
  
 const Sitemap = (): MetadataRoute.Sitemap => ([
   {
-    url: 'https://algrith.com',
+    url: 'https://algrith.com/auth/resend-verification',
     changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 1
+    priority: 0.8
+  },
+  {
+    url: 'https://algrith.com/auth/forgot-password',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 0.8
+  },
+  {
+    url: 'https://algrith.com/auth/password-reset',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 0.8
   },
   {
     url: 'https://algrith.com/terms-of-service',
     changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 0.8
+    priority: 1.0
   },
   {
     url: 'https://algrith.com/privacy-policy',
     changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 0.8
+    priority: 1.0
   },
   {
     url: 'https://algrith.com/refund-policy',
     changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 0.8
+    priority: 1.0
   },
   {
     url: 'https://algrith.com/how-it-works',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 1.0
+  },
+  {
+    url: 'https://algrith.com/auth/sign-up',
     changeFrequency: 'monthly',
     lastModified: new Date(),
     priority: 0.8
   },
   {
     url: 'https://algrith.com/contact-us',
-    changeFrequency: 'weekly',
+    changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 0.5
+    priority: 1.0
+  },
+  {
+    url: 'https://algrith.com/auth/verify',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 1.0
   },
   {
     url: 'https://algrith.com/about-us',
-    changeFrequency: 'weekly',
+    changeFrequency: 'monthly',
     lastModified: new Date(),
-    priority: 0.5
+    priority: 1.0
+  },
+  {
+    url: 'https://algrith.com/pricing',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 1.0
+  },
+  {
+    url: 'https://algrith.com/auth',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 1.0
+  },
+  {
+    url: 'https://algrith.com',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 1.0
   }
 ]);
 
