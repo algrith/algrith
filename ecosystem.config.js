2bd6655ce4a28a9e05498fb585fa71740bb1af77
Uwakmfon Akpan <astongemmy@gmail.com>
2026-06-23 12:59:26 +0100

    Updated ecosystem.config file.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   apps: [
     {
+      args: "server.js",
       name: "nextjs",
       script: "node",
-      args: "server.js", // Next.js standalone server
-      env: { PORT: 3000, NODE_ENV: "production" },
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
     },
     {
+      args: "tsx --env-file=./.env socket-server/index.ts",
       name: "socket",
       script: "npx",
-      args: "tsx socket-server/index.ts",
-      env: { PORT: 3001, NODE_ENV: "production" },
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
     },
   ],
 };
